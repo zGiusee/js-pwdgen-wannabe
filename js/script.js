d413fd5ea8172c3b71957f120755597b7b6c167d
zGiusee <giusebelforte@gmail.com>
2023-11-17 14:39:53 +0100

    [UPDATE] removede console.log as useless component

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 
 let name = prompt ("Qual'è il tuo Nome?");
-console.log(name);
 
 let cognome = prompt ("Qual'è il tuo Cognome?");
-console.log(cognome);
 
 let colore_preferito = prompt ("Qual'è il tuo colore preferito?");
-console.log(colore_preferito);
 
 let user_info = name + cognome + colore_preferito + `23`;
-console.log(user_info)
 
 document.getElementById(`user_info_d`).innerHTML = user_info;
-console.log(`user_info_d`);
