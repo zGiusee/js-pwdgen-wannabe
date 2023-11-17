7761a417c0dede2bfa4776534e3378901c1a4aea
zGiusee <giusebelforte@gmail.com>
2023-11-17 14:44:06 +0100

    [UPDATE] used " and not ` in 23 info

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ let cognome = prompt ("Qual'è il tuo Cognome?");
 
 let colore_preferito = prompt ("Qual'è il tuo colore preferito?");
 
-let user_info = name + cognome + colore_preferito + `23`;
+let user_info = name + cognome + colore_preferito + "23";
 
 document.getElementById(`user_info_d`).innerHTML = user_info;
