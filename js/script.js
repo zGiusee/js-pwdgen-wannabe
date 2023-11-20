4b77258be529edf7fdac73482dbf670ce202d0b5
zGiusee <giusebelforte@gmail.com>
2023-11-20 09:52:52 +0100

    [UPDATE] class correction

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+// Chiedi all'utente il suo nome
+let name = prompt("Qual'è il tuo Nome?");
 
-let name = prompt ("Qual'è il tuo Nome?");
+// Chiedi all'utente il suo congnome
+let cognome = prompt("Qual'è il tuo Cognome?");
 
-let cognome = prompt ("Qual'è il tuo Cognome?");
+// Chiedi all'utente il suo colore preferito
+let colore_preferito = prompt("Qual'è il tuo colore preferito?");
 
-let colore_preferito = prompt ("Qual'è il tuo colore preferito?");
-
+// Generazione della variabile
 let user_info = name + cognome + colore_preferito + "23";
+
 
 document.getElementById("user_info_d").innerHTML = user_info;
